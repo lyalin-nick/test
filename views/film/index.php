1b61d9eb71e790f126420600362f6081f8fd63f4
lyalin-nick <52660006+lyalin-nick@users.noreply.github.com>
2019-11-18 10:24:42 +0300

    Красивые даты в сеансах

--- views/film/index.php
+++ views/film/index.php
@@ -14,6 +14,7 @@ $this->title = 'Test Yii Application';
         <div class="row">
 
                 <?php foreach ($cinemasession as $ses):?>
+                    <?php if($date!=$ses->date) echo "<br><h2>Сеансы ".Yii::$app->formatter->asDate($ses->date, 'dd.MM.yyyy')."</h2>"; $date=$ses->date ?>
                     <div class="well">
                         <img src="<?= $ses->getFilmImage() ?>" alt="" style="width: 150px">
                         <h2><a href=<?= Url::toRoute(['film/view', 'id' => $ses->film_id ]); ?>><?= $ses->getFilmTitle()?></a></h2>
